7254ff3f1fd9fd261c29f12bf00c96eed30c1e5f
FURUNDA-IOS002 <furunda-ios002@FURUNDA-IOS002deMac-mini.local>
2017-04-21 12:01:14 +0800

    Initial Commit

---
+++ gitless/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitless
+//
+//  Created by FURUNDA-IOS002 on 2017/4/21.
+//  Copyright © 2017年 FURUNDA-IOS002. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitless/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitless
+//
+//  Created by FURUNDA-IOS002 on 2017/4/21.
+//  Copyright © 2017年 FURUNDA-IOS002. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
